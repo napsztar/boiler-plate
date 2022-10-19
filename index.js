b96b61792c177f5a430d087a704c347961c24467
Napsztar <napsztar@gmail.com>
2022-10-19 15:55:13 +0900

    Add login

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
 const {User} = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -11,8 +16,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 //application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://napsztar:1234@boilerplate.yhnuozx.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
     .catch(err=>console.log(err))
@@ -30,6 +37,38 @@ app.post('/register',(req,res)=>{
             success:true
         })
     })
+})
+
+app.post('/login',(req,res)=>{
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess:false,
+                message:"제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+
+        user.comparePassword(req.body.password, (err,isMatch)=>{
+            if(!isMatch)
+                return res.json({loginSuccess:false, message:"비밀번호가 틀렸습니다."})
+
+            //비밀번호까지 일치한다면 토큰을 생성한다.
+            user.generateToken((err,user)=>{
+            if(err) return res.status(400).send(err);
+            //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지..
+                res.cookie("x_auth",user.token)
+                    .status(200)
+                    .json({loginSuccess:true,userId:user._id})
+
+            })
+        })
+    })
+
+
+
 })
 
 app.listen(port,()=> console.log(`Example app listening on port ${port}`))
