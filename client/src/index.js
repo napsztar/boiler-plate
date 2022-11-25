6bf7cfadc16043ece12e5e3d19a72461e4485f39
Napsztar <napsztar@gmail.com>
2022-11-26 05:12:28 +0900

    Add 미들웨어 세팅

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, createStore} from "redux";
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import rootReducer from './_reducers'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 root.render(
+    <Provider store={createStoreWithMiddleware(rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                    window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+    >
   <React.StrictMode>
     <App />
   </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
