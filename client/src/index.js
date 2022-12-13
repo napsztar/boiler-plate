1a6a0876f79516f356463bd0a1d919c8e3e3bf1d
Napsztar <napsztar@gmail.com>
2022-12-13 23:06:49 +0900

    로그인 페이지 완성

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 root.render(
     <Provider store={createStoreWithMiddleware(rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
                     window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
     >
