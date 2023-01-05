54d60de8a67b00de772ffcca69af3f768e208817
Napsztar <napsztar@gmail.com>
2023-01-05 23:20:07 +0900

    react-router-dom 버전으로 인한 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import Auth from "../../../hoc/auth";
 
 function LandingPage() {
 
@@ -29,4 +30,4 @@ function LandingPage() {
         </div>
     )
 }
-export default LandingPage
+export default Auth(LandingPage,null)
